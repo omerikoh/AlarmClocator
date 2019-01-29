38317e81810bb7579ea40a200d94c201680d3883
omerikoh <omerikoh@hasson.org.il>
2019-01-29 19:59:19 +0200

    Improving the location searching

---
+++ AlarmClocator/LocationSearchTable.swift
@@ -0,0 +1,102 @@
+//
+//  LocationSearchTable.swift
+//  AlarmClocator
+//
+//  Created by Omer Hasson on 29/01/2019.
+//  Copyright © 2019 Omer Hasson. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationSearchTable: UITableViewController {
+    
+
+    weak var handleMapSearchDelegate: HandleMapSearch?
+    var matchingItems: [MKMapItem] = []
+    var mapView: MKMapView?
+    
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        
+        // For example put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil &&
+            selectedItem.thoroughfare != nil) ? " " : ""
+        
+        // Put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) &&
+            (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        
+        // For example put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil &&
+            selectedItem.administrativeArea != nil) ? " " : ""
+        
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        
+        return addressLine
+    }
+    
+}
+
+// This extention 
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+    
+}
+
+extension LocationSearchTable {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
+        return cell
+    }
+    
+}
+
+extension LocationSearchTable {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+
--- AlarmClocator/MapViewController.swift
+++ AlarmClocator/MapViewController.swift
@@ -10,27 +10,139 @@ import UIKit
 import MapKit
 import CoreLocation
 
+protocol HandleMapSearch: class {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
 
 class MapViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate {
     
     
     @IBOutlet weak var myMapView: MKMapView!
     
+    // These variables are being used in the searching function
+    var selectedPin: MKPlacemark?
+    var resultSearchController: UISearchController!
+
     var location: CLLocationCoordinate2D?
     var locationName: String?
     
-    @IBAction func searchButton(_ sender: Any) {
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchBar.delegate = self
-        present(searchController, animated: true, completion: nil)
-    }
+    
+    
     
     @IBAction func donePressed(_ sender: Any) {
         performSegue(withIdentifier: "unwindToAddScreen", sender: self)
     }
     
+    // This function shows the user's location at the moment he's openning the map view
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: myLocation, span: span)
+        myMapView.setRegion(region, animated: true)
+        
+        self.myMapView.showsUserLocation = true
+        self.manager.stopUpdatingLocation()
+    }
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error)")
+    }
+    
+    let manager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+        
+        addMapTrackingButton()
+        
+        // Lines 132-144 build the search bar and the list of places
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController.searchResultsUpdater = locationSearchTable as UISearchResultsUpdating
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        resultSearchController.hidesNavigationBarDuringPresentation = false
+        resultSearchController.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        locationSearchTable.mapView = myMapView
+        locationSearchTable.handleMapSearchDelegate = self as HandleMapSearch
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /// This function adds the button that let the user to focus on his location
+    func addMapTrackingButton(){
+        let buttonItem = MKUserTrackingButton(mapView: myMapView)
+        buttonItem.frame = CGRect(origin: CGPoint(x: 336.5, y: 45), size: CGSize(width: 35, height: 35))
+        
+        myMapView.addSubview(buttonItem)
+    }
+    
+    @objc func getDirections(){
+        guard let selectedPin = selectedPin else { return }
+        let mapItem = MKMapItem(placemark: selectedPin)
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        mapItem.openInMaps(launchOptions: launchOptions)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //the chosen location
+        guard let destinationViewController = segue.destination as? AddScreen else {return}
+        
+        if self.location != nil {
+            destinationViewController.destinationName = self.locationName
+            destinationViewController.destination = self.location
+            destinationViewController.tableView.reloadData()
+        }
+    }
+ 
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MapViewController: HandleMapSearch {
+    
+    func dropPinZoomIn(placemark: MKPlacemark){
+        /*/ cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        myMapView.removeAnnotations(myMapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city) \(state)"
+        }
+ */
+        /*
+        myMapView.addAnnotation(annotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
+        myMapView.setRegion(region, animated: true)
+        */
+        //create the search request
+        
         //Ignoring user
         UIApplication.shared.beginIgnoringInteractionEvents()
         
@@ -40,19 +152,11 @@ class MapViewController: UIViewController, UISearchBarDelegate, CLLocationManage
         activityIndicator.center = self.view.center
         activityIndicator.hidesWhenStopped = true
         activityIndicator.startAnimating()
-        
-        self.view.addSubview(activityIndicator)
-        
-        //hide search bar
-        searchBar.resignFirstResponder()
-        dismiss(animated: true)
-        
-        //create the search request
         let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = searchBar.text
+        searchRequest.naturalLanguageQuery = placemark.title
         
         //adding the requested location to a variable
-        self.locationName = searchBar.text
+        self.locationName = placemark.name
         
         let activeSearch = MKLocalSearch(request: searchRequest)
         
@@ -81,76 +185,25 @@ class MapViewController: UIViewController, UISearchBarDelegate, CLLocationManage
                 
                 //create annotation
                 let annotation = MKPointAnnotation()
-                annotation.title = searchBar.text
-                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                annotation.title = placemark.title
+                annotation.coordinate = placemark.coordinate
                 self.myMapView.addAnnotation(annotation)
+                
+                if let city = placemark.locality,
+                    let state = placemark.administrativeArea {
+                    annotation.subtitle = "\(city) \(state)"
+                }
                 
                 //zooming-in on annotation
                 let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
                 
                 //adding the requested location coordinates to a variable
                 self.location = coordinate
-                let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1) //means how much we want to be assumed in
+                let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01) //means how much we want to be assumed in
                 let region = MKCoordinateRegion(center: coordinate, span: span)
                 self.myMapView.setRegion(region, animated: true)
                 
             }
         }
-        
     }
-    
-    @IBOutlet weak var SearchButton: UIBarButtonItem!
-
-    let manager = CLLocationManager()
-    
-    //this function controls the attributes of the user's location
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
-    {
-        let location = locations[0]
-        
-        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: myLocation, span: span)
-        myMapView.setRegion(region, animated: true)
-        
-        self.myMapView.showsUserLocation = true
-        
-}
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.startUpdatingLocation()
-        
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //the chosen location
-        guard let destinationViewController = segue.destination as? AddScreen else {return}
-        
-        if self.location != nil {
-            destinationViewController.destinationName = self.locationName
-            destinationViewController.destination = self.location
-            destinationViewController.tableView.reloadData()
-        }
-    }
- 
- 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
